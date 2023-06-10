4ac6f2ea19c5c75afef9541f2074c2440ff26074
Isaac Abarca <abarcai532@gmail.com>
2023-06-10 10:39:39 -0600

    gh-pages agregado

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+
+# git push -f git@ginthub.com:Isaac-Abarca/tarea_landing_page.git main:gh-pages
